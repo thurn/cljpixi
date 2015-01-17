d62937740177a0f53e0e838fb6b3529829d66e7b
Derek Thurn <derek@fake.email>
2015-01-17 11:30:39 -0800

    updated expression docs

--- src/tin/new.cljs
+++ src/tin/new.cljs
@@ -69,15 +69,14 @@
     (reset! last-timestamp timestamp))
   (js/requestAnimFrame animate-loop-fn))
 
+(declare handle-message)
+
 (defn render-message-loop
   [{:as engine render-channel :render-channel dispatch-map :dispatch-map}]
-  (go
+  (go ;; TODO: Process messages in batches.
     (loop [message (<! render-channel)]
-      (let [result ((dispatch-map (first message)) engine message)]
-        (match result
-               {:chan chan} (<! chan)
-               :else nil)
-        (recur (<! render-channel))))))
+      (handle-message engine message)
+      (recur (<! render-channel)))))
 
 (defn initialize
   "Takes an EngineConfiguration and creates a new Engine instance. You should
@@ -100,3 +99,57 @@
     (animate-loop engine)
     (render-message-loop engine)
     engine))
+
+(defn- handle-message
+  "Process a render message by dispatching to the appropriate handler function.
+   A handler can optionally return a chan object, if one is returned message
+   processing will be halted until a read on the channel completes."
+  [engine message])
+
+(comment
+  ; Top-level messages:
+  ; create display objects
+  [:render & object-exprs]
+  ; update existing display objects
+  [:update identifier properties {:function f}]
+  ; load assets
+  [:load identifier & assets]
+  ; start an animation
+  [:animate & animation-exprs]
+  ; listen for input events on a display object
+  [:subscribe source :to destination :query query :event event-expr]
+  ; stop listening for input events
+  [:unsubscribe source :from destination]
+  ; remove everything from the stage
+  [:clear]
+
+  ; Object expressions
+  [:container identifier options & children]
+  [:sprite identifier texture options]
+  [:tiling-sprite identifier texture width height options]
+  [:movie-clip identifier textures options]
+  [:point x y]
+  [:texture :image path]
+  [:texture :canvas canvas]
+  [:texture :frame frame-id]
+
+  ; Animation expressions
+  [:tween properties {:function f :duration d :ease e}]
+  [:play-clip frame?]
+  [:stop-clip frame?]
+  [:then message] ; Push arbitrary message onto the render queue.
+
+  ; Event expressions
+  [:pan :threshold 10]
+  [:pinch :pointers 2]
+  [:press :time 500]
+  [:rotate :threshold 10]
+  [:swipe :velocity 0.65]
+  [:tap :taps 1]
+  [:mouse-over]
+  [:mouse-out]
+  [:click-start]
+  [:click-end]
+  [:click-end-outside]
+  [:on-load]
+)
